efc9a3bfd2e9e774b037621f485520fcffdd38e4
Regis d'Aubarede <regis.aubarede@gmail.com>
2017-08-31 16:12:55 +0200

    update

--- srv_bash.sh
+++ srv_bash.sh
@@ -2,6 +2,8 @@
 echo  "CLEAR"
 echo "POS, 40 / 40 /// Hello ! "
 echo "PLINE, #888, #888, 10, 0 / 50, 200 / 50"
+echo "END"
+
 sleep 3
 while :; do 
   echo "CLEAR"
@@ -9,5 +11,6 @@ while :; do
   echo "POS, 10 / 15 /// Date: $(date | awk ' {printf $4}')"
   echo "POS, 10 / 30 /// ls: $(ls -1 | wc -l)"
   echo "PLINE, #888, #888, 10, 0/50, 200/50, 200/60, 0/60"
+  echo "END"
   sleep 1
 done
