4b07af742837549d1003be0c9aa4496c58a5408c
Regis d'Aubarede <regis.aubarede@gmail.com>
2017-09-11 16:19:08 +0200

    backgroung layout, force window size

--- client.rb
+++ client.rb
@@ -1,5 +1,5 @@
 #########################################################################################
-# sshvector: client.rb : prototype of 'client' sshvector
+# netvector: client.rb : prototype of 'client' sshvector (POC, so => ruby  ; could be .net/Qt/java)
 #      Establish a ssh (or tcp or local-exec) connection, 
 #      Run a very simple serveur prog ; get STDOUT of this prog.
 #      Manage graphique interface with this stdout
@@ -23,19 +23,18 @@
 #    END order grapic sequence, do refresh on client side
 #    CLEAR reset the list of vector, on client side
 #
-#  echo "CLEAR" # start of refresh
+#  echo "CLEARBG"                            # start backgound vector list
+#  echo "CLEAR"                              # start vector list
 #  echo "POS,10/15 /// Date: $(date)"        # x/y position , texte
 #  echo "RECT,#CCC, #CCC, 0, 0/0, 200/100"   # bgcolor fgcolor border-width x y w h
 #  echo "PLINE, #888, #888, 10, 0/50, 200/50, 200/60, 0/60" # ; bgcolor fgcolor border-width x y x y...
 #  echo "OVAL, #888, #888, 10, 0/50, 200"    # ; bgcolor fgcolor border-width x-center y-center r
 #  echo "END"                                #  end of list , refresh
+#  echo "ENDBG"                              #  end of background list
 # 
 ##########################################################################################
-require 'Ruiby'
-require 'minitcp'
-require_relative  'utils'
 
-if ARGV.size!=0 && ARGV.size<5
+if  ARGV.size<5
  puts "Usage:  \n   >ruby #{$0} ip-host      user pass|noport ssh|tcp|local command..."
  puts "Exemple:\n   >ruby #{$0} 192.168.0.1 root 1234 ssh ruby srv_clock.rb 22"
  puts %{   >ruby client.rb ab root 1234 ssh ruby -e "'$stdout.sync=true;loop {puts \"CLEAR;POS,0,20///# {Time.now};END\" ;sleep 3}'"}
@@ -43,17 +42,22 @@ if ARGV.size!=0 && ARGV.size<5
  exit(1)
 end
 
-$host= ARGV[0] || "10.203.76.202" 
-$user=ARGV[1] || "actemium" 
-$passwd= ARGV[2] || "Actemium111" 
-$PROTO= ARGV[3] || "ssh"
+require 'Ruiby'
+require 'minitcp'
+require_relative  'utils'
+
+$host= ARGV[0]
+$user=ARGV[1]
+$passwd= ARGV[2] 
+$PROTO= ARGV[3]
 $port=$passwd.to_i if $PROTO=="tcp"
-$cmd=ARGV[4..-1].join(" ") || "ruby server.rb ps"
+$cmd=ARGV[4..-1].join(" ")
 
 class SSHClient
   def initialize(app)
     @app=app
-    @lvect=[]
+    @lvect=[[],[]]
+    @nolayout=nil
   end
   ################## Net
   def connect()
@@ -73,7 +77,8 @@ class SSHClient
     end
   end
   def net_connected(channel) 
-    @lvect=[]
+    @lvect=[[],[]]
+    @nolayout=nil
     @app.set_label("Connected") 
   end
   
@@ -83,7 +88,8 @@ class SSHClient
     @app.after(1000) { ici.reset    }
   end
   def reset()
-    @lvect=[]
+    @lvect=[[],[]]
+    @nolayout=nil
     $app.update
   end
   def net_receive_data(data0)
@@ -92,33 +98,41 @@ class SSHClient
       data,txt=data.split(%r{\s*///\s*},2) if data[0,3]=="POS"
       cmd,*params=data.chomp.split(%r{\s*[,/]\s*})
       case(cmd)
-        when "CLEAR" then @lvect= []
-        when "POS"   then @lvect << [:pos,{x: params[0].to_i, y: params[1].to_i,t: (txt || params[2])}]
-        when "RECT"  then @lvect << [:rect,{x: params[3].to_i,y: params[4].to_i,w: params[5].to_i,h: params[6].to_i,cfg:params[0],cbg: params[1],ep: params[2].to_i}]
-        when "OVAL"  then @lvect << [:oval,{cfg:params[0],cbg: params[1],ep: params[2].to_i,x: params[3].to_i,y: params[4].to_i,r: params[5].to_i}]
-        when "PLINE"  then @lvect << [:plin,{cfg:params[0],cbg: params[1],ep: params[2].to_i,lxy:params[3..-1].map {|a|a.to_i}}]
-        when "POLYG"  then @lvect << [:polyg,{cfg:params[0],cbg: params[1],ep: params[2].to_i,lxy:params[3..-1].map {|a|a.to_i}}]
+        when "CLEARBG" 
+          @nolayout=0
+          @lvect[@nolayout]= []
+        when "CLEAR" 
+          @nolayout=1
+          @lvect[@nolayout]= []
+        when "DIM"   then (gui_invoke {@app.set_size(params[0].to_i,params[1].to_i) }) if @layout==0
+        when "POS"   then @lvect[@nolayout] << [:pos,{x: params[0].to_i, y: params[1].to_i,t: (txt || params[2])}]
+        when "RECT"  then @lvect[@nolayout] << [:rect,{x: params[3].to_i,y: params[4].to_i,w: params[5].to_i,h: params[6].to_i,cfg:params[0],cbg: params[1],ep: params[2].to_i}]
+        when "OVAL"  then @lvect[@nolayout] << [:oval,{cfg:params[0],cbg: params[1],ep: params[2].to_i,x: params[3].to_i,y: params[4].to_i,r: params[5].to_i}]
+        when "PLINE"  then @lvect[@nolayout] << [:plin,{cfg:params[0],cbg: params[1],ep: params[2].to_i,lxy:params[3..-1].map {|a|a.to_i}}]
+        when "POLYG"  then @lvect[@nolayout] << [:polyg,{cfg:params[0],cbg: params[1],ep: params[2].to_i,lxy:params[3..-1].map {|a|a.to_i}}]
+        when "ENDBG"   then @nolayout=1
         when "END"   then @app.update
         else
             puts "unknown line : #{data.inspect}"
-            @lvect=[] if @lvect.size>20 
-            @lvect << [:pos,{x: 50, y: (@lvect.size+1)*10,t: "???>" + data}]
+            @lvect[@nolayout]=[] if @lvect[@nolayout].size>20 
+            @lvect[@nolayout] << [:pos,{x: 50, y: (@lvect[@nolayout] .size+1)*10,t: "???>" + data}]
             @app.update
       end
     end  
   rescue
-    @lvect=[] if @lvect.size>30 
-    @lvect << [:pos,{x: 0, y: (@lvect.size+1)*10,t: "   >" + $!.to_s}]
-    @lvect << [:pos,{x: 0, y: (@lvect.size+1)*10,t: "???>" + data}]
+    @nolayout=1 unless @nolayout
+    @lvect[@nolayout]=[] if @lvect[@nolayout].size>30 
+    @lvect[@nolayout] << [:pos,{x: 0, y: (@lvect[@nolayout].size+1)*10,t: "   >" + $!.to_s}]
+    @lvect[@nolayout] << [:pos,{x: 0, y: (@lvect[@nolayout].size+1)*10,t: "???>" + data}]
     @app.update
     puts $!
   end
   
   #################  Draw
   def redraw(w,cr)
-    return unless @lvect.size>0
+    return unless @lvect.size>0 || @lvect[0].size+@lvect[1].size==0
     @app.ctx_font(cr,"Courier new bold 12",12)
-    @lvect.each do |(cmd,h)|
+    @lvect.each do |layout| layout.each do |(cmd,h)|
       begin
         case(cmd)
           when :pos then w.draw_text(h[:x],h[:y],h[:t],1,"#044")
@@ -132,7 +146,7 @@ class SSHClient
       rescue
         puts "#{cmd}: Error #{$!} :\n       #{h}"
       end
-    end
+    end end
   end
   def mouse_release(w,x,y,e) 
   end
@@ -157,9 +171,6 @@ Ruiby.app width:200, height:250, title: "S" do
   @chrome=true
   stack do
     flowi {pclickable(proc {@chrome=! @chrome; self.chrome(@chrome)}) { @lab=label(" <status>  ")  }}
-    #flowi {
-    #    buttoni("Connection...") { @cli.connect() }
-    #}
     @cv=canvas(100,20) {
       on_canvas_draw { |w,ctx| @cli.redraw(w,ctx) }
       on_canvas_button_release {|w,e,o| @cli.mouse_release(w,e.x,e.y,e) }
--- srv_clock.rb
+++ srv_clock.rb
@@ -1,39 +1,66 @@
+####################################################################################
+#  srv_clock.rb : demo of a  client netvector
+#  use background layer (heavy clock design) and foreground layer (clock needle)
+####################################################################################
+
+# no any require :)
+
+# restart if file is modified
 $lmtime= File.mtime(__FILE__) ; Thread.new {loop { exit(0) if File.mtime(__FILE__)!=$lmtime; sleep 3}}
 
-def draw(c,r,color,coef,ratio)
+def draw_needle(c,r,color,coef,ratio)
  a=(ratio+0.75)*2.0*Math::PI
  sin,cos= 1.0*r*coef*Math.sin(a), 1.0*r*coef*Math.cos(a)
  c1=[c[0]+cos,c[1]+sin]
  puts "PLINE, #{color}, #{color}, 2, #{c[0]} / #{c[1]}, #{c1[0]} / #{c1[1]}"
 end
+
 def clock()
   t=Time.now
   s,m,h=t.sec,t.min,t.hour
-  c,r=[150,50],20
-  puts "OVAL, #AAA, #FEE, 3, #{c[0]} / #{c[1]}, #{r*1.2}"
-  draw(c,r,"#AAB",0.5,1.0*((h+(m/60.0))%12)/12.0)
-  draw(c,r,"#ABA",0.8,1.0*((m+s/60.0)/60.0))
-  draw(c,r,"#BAA",1.0,1.0*s/60.0)
-  puts "OVAL, #AAA, #AAA, 1, #{c[0]} / #{c[1]}, 3"
+  c,r=[150.0,50.0],30.0
+  draw_needle(c,r,"#AAB",0.5,1.0*((h+(m/60.0))%12)/12.0)
+  draw_needle(c,r,"#ABA",0.8,1.0*((m+s/60.0)/60.0))
+  draw_needle(c,r,"#BAA",1.0,1.0*s/60.0)
   #puts "POS, 130, 80, #{h}:#{m}:#{s}"
 end 
 
-puts "CLEAR"
-puts "POS, 40 / 40 /// Hello ! "
-puts "PLINE, #888, #888, 10, 0, 50, 200, 50"
-puts "END"
-$stdout.flush
-sleep 3
+def clock_init()
+  c,r=[150.0,50.0],30.0
+  puts "OVAL, #AAA, #FEE, 3, #{c[0]} / #{c[1]}, #{r*1.01}"
+  puts "OVAL, #AAA, #AAA, 1, #{c[0]} / #{c[1]}, 3"
+  # graduations
+  (1..60).each  do |t|
+    a=((1.0*t/60.0)+0.75)*2.0*Math::PI
+    q=((t%5)==0) 
+    coef=(!q) ? 0.90 : 0.80
+    sin,cos= 1.0*r*coef*Math.sin(a), 1.0*r*coef*Math.cos(a)
+    c1=[c[0]+cos,c[1]+sin]
+
+    coef=1.0
+    sin,cos= 1.0*r*coef*Math.sin(a), 1.0*r*coef*Math.cos(a)
+    c2=[c[0]+cos,c[1]+sin]
+    color="#AAA"
+    puts "PLINE, #{color}, #{color}, #{q ? 3 : 1}, #{c1[0]} / #{c1[1]}, #{c2[0]} / #{c2[1]}"
+  end
+end 
+
+puts "CLEARBG"
+puts "DIM,  200  / 100"
+puts "RECT, #CCC, #CCC, 0 , 0 / 0 , 200  / 100"
+puts "PLINE, #888, #888, 10, 0 / 70, 200 / 70, 200 / 80, 0 / 80"
+clock_init()
+puts "ENDBG"
+
 hh=0
 loop {
   puts "CLEAR"
-  puts "RECT, #CCC, #CCC, 0 , 0 / 0 , 200  / 100"
   hh= (hh + 1) % 100
-  puts "PLINE, #888, #888, 10, 0 / #{hh}, 200 / #{hh}, 200 / #{hh+10}, 0 / #{hh+10}"
   puts "POS, 10 / 15 /// Date: #{Time.now.to_s.split(/ /)[1]}"
   puts "POS, 10 / 30 /// ls: #{Dir.glob("*").size}"
-  puts "END"
   clock()
+  puts "END"
   $stdout.flush
   sleep 1
 }
+
